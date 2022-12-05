4cc13fe95875284b3d5ba4817be5110727667b52
jonahhowell <jonahowell800@gmail.com>
2022-12-05 15:37:49 -0500

    Wrote a lot of helper functions, completed playRound

--- script.js
+++ script.js
@@ -5,6 +5,34 @@ function getComputerChoice() {
     return 'Scissors';
 }
 
+function playRound(playerSelection, computerSelection) {
+    playerSelection = formatString(playerSelection);
+    computerSelection = formatString(computerSelection);
+    if (playerSelection === 'Rock') {
+        if (computerSelection === 'Scissors') return winText(playerSelection, computerSelection);
+        if (computerSelection === 'Paper') return loseText(playerSelection, computerSelection);
+        return 'Tie!';
+    }
+    if (playerSelection === 'Paper') {
+        if (computerSelection === 'Rock') return winText(playerSelection, computerSelection);
+        if (computerSelection === 'Scissors') return loseText(playerSelection, computerSelection);
+        return 'Tie!';
+    }
+    if (playerSelection === 'Scissors') {
+        if (computerSelection === 'Paper') return winText(playerSelection, computerSelection);
+        if (computerSelection === 'Rock') return loseText(playerSelection, computerSelection);
+        return 'Tie!';
+    }
+}
+
+function winText(player, computer) {
+    return "You win! " + player + " beats " + computer + "!";
+}
+
+function loseText(player, computer) {
+    return "You lose! " + computer + " beats " + player + "!";
+}
+
 function formatString(str) {
     if (str.length == 0) return "";
     firstC = str.slice(0, 1).toUpperCase();
@@ -12,4 +40,4 @@ function formatString(str) {
     return firstC + remainder;
 }
 
-console.log(formatString("r"));
+console.log(playRound(getComputerChoice(), getComputerChoice()));
