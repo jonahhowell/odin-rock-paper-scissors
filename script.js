beff2884f63123af27a8c64f3473c06085b8f42c
jonahhowell <jonahowell800@gmail.com>
2022-12-04 20:51:37 -0500

    Wrote computer choice func

---
+++ script.js
@@ -0,0 +1,6 @@
+function getComputerChoice() {
+    const choice = Math.floor(Math.random() * 3);
+    if (choice === 0) return 'rock';
+    if (choice === 1) return 'paper';
+    return 'scissors';
+}
