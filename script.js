a2e2377672b0aefd81d4b4e4a350949697183f70
jonahhowell <jonahowell800@gmail.com>
2022-12-05 15:26:16 -0500

    Wrote function to format string into proper form

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function getComputerChoice() {
     const choice = Math.floor(Math.random() * 3);
-    if (choice === 0) return 'rock';
-    if (choice === 1) return 'paper';
-    return 'scissors';
+    if (choice === 0) return 'Rock';
+    if (choice === 1) return 'Paper';
+    return 'Scissors';
 }
+
+function formatString(str) {
+    if (str.length == 0) return "";
+    firstC = str.slice(0, 1).toUpperCase();
+    remainder = str.slice(1, str.length).toLowerCase();
+    return firstC + remainder;
+}
+
+console.log(formatString("r"));
